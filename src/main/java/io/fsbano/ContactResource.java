e25e3d2ed2d25d5d3d1bf0274ec6f44f96dc3dfb
Fabio Sbano <fsbano@yahoo.com>
2022-06-12 15:35:21 -0300

    jax-rs-resource with panache - CRUD

--- src/main/java/io/fsbano/ContactResource.java
+++ src/main/java/io/fsbano/ContactResource.java
@@ -1,12 +1,17 @@
 package io.fsbano;
 
 import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -30,6 +35,12 @@ public class ContactResource {
         return Contact.listAll();
     }
 
+    @GET
+    @Path("{id}")
+    public Uni<Contact> getSingle(UUID id) {
+        return Contact.findById(id);
+    }
+
     @POST
     public Uni<Response> create(Contact contact) {
         if (contact == null || contact.id != null) {
@@ -39,4 +50,30 @@ public class ContactResource {
         return Panache.withTransaction(contact::persist)
                     .replaceWith(Response.ok(contact).status(CREATED)::build);
     }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> update(UUID id, Contact contact) {
+        if (contact == null || contact.name == null) {
+            throw new WebApplicationException("Contact name was not set on request.", 422);
+        }
+
+        return Panache
+                .withTransaction(() -> Contact.<Contact> findById(id)
+                    .onItem().ifNotNull().invoke(entity -> entity.name = contact.name)
+                    .onItem().ifNotNull().invoke(entity -> entity.email = contact.email)
+                )
+                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(UUID id) {
+        return Panache.withTransaction(() -> Contact.deleteById(id))
+                .map(deleted -> deleted
+                        ? Response.ok().status(NO_CONTENT).build()
+                        : Response.ok().status(NOT_FOUND).build());
+    }
+
 }
