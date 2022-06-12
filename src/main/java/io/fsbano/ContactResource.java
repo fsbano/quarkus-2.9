79593aab40fdb5f35038927084538cb8b7daf06d
Fabio Sbano <fsbano@yahoo.com>
2022-06-12 13:18:43 -0300

    GET Contacts

--- src/main/java/io/fsbano/ContactResource.java
+++ src/main/java/io/fsbano/ContactResource.java
@@ -2,6 +2,9 @@ package io.fsbano;
 
 import static javax.ws.rs.core.Response.Status.CREATED;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,12 +19,18 @@ import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
 
 @Path("/api/contact")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ContactResource {
 
     private static final Logger LOGGER = Logger.getLogger(ContactResource.class.getName());
 
+    @GET
+    public Uni<List<Contact>> get() {
+        return Contact.listAll();
+    }
+
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> create(Contact contact) {
         if (contact == null || contact.id != null) {
             throw new WebApplicationException("Id was invalidly set on request.", 422);
